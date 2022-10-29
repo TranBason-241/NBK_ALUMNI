0ebb2943a19b6a5e4b826ca9115a45ede9122a5f
tranbason241@gmail.com <tranbason241@gmail.com>
2022-10-29 20:57:50 +0700

    first commit

---
+++ src/components/_external-pages/landing/LandingHero.tsx
@@ -0,0 +1,157 @@
+import { Icon } from '@iconify/react';
+import { motion } from 'framer-motion';
+import flashFill from '@iconify/icons-eva/flash-fill';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { styled } from '@material-ui/core/styles';
+import { Button, Box, Link, Container, Typography, Stack, StackProps } from '@material-ui/core';
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths';
+//
+import { varFadeIn, varFadeInUp, varWrapEnter, varFadeInRight } from '../../animate';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(motion.div)(({ theme }) => ({
+  position: 'relative',
+  backgroundColor: theme.palette.grey[400],
+  [theme.breakpoints.up('md')]: {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    position: 'fixed',
+    alignItems: 'center'
+  }
+}));
+
+const ContentStyle = styled((props: StackProps) => <Stack spacing={5} {...props} />)(
+  ({ theme }) => ({
+    zIndex: 10,
+    maxWidth: 520,
+    margin: 'auto',
+    textAlign: 'center',
+    position: 'relative',
+    paddingTop: theme.spacing(15),
+    paddingBottom: theme.spacing(15),
+    [theme.breakpoints.up('md')]: {
+      margin: 'unset',
+      textAlign: 'left'
+    }
+  })
+);
+
+const HeroOverlayStyle = styled(motion.img)({
+  zIndex: 9,
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  position: 'absolute'
+});
+
+const HeroImgStyle = styled(motion.img)(({ theme }) => ({
+  top: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 8,
+  width: '100%',
+  margin: 'auto',
+  position: 'absolute',
+  [theme.breakpoints.up('lg')]: {
+    right: '8%',
+    width: 'auto',
+    height: '48vh'
+  }
+}));
+
+// ----------------------------------------------------------------------
+
+export default function LandingHero() {
+  return (
+    <>
+      <RootStyle initial="initial" animate="animate">
+        <HeroOverlayStyle alt="overlay" src="/static/overlay.svg" variants={varFadeIn} />
+
+        <HeroImgStyle
+          alt="hero"
+          src="/static/home/background_langding.png"
+          variants={varFadeInUp}
+        />
+
+        <Container maxWidth="md">
+          <ContentStyle>
+            <motion.div variants={varFadeInRight}>
+              <Typography variant="h1" sx={{ color: 'common.white' }}>
+                <br /> NGUYEN BINH KHIEM
+                <Typography component="span" variant="h1" sx={{ color: 'primary.main' }}>
+                  &nbsp;A
+                  <Typography component="span" variant="h1" sx={{ color: 'primary.main' }}>
+                    LUMNI
+                  </Typography>
+                </Typography>
+              </Typography>
+            </motion.div>
+
+            <motion.div variants={varFadeInRight}>
+              <Typography variant="h3" sx={{ color: 'common.white' }}>
+                Mạng lưới cựu sinh viên THPT Nguyễn Bỉnh Khiêm
+              </Typography>
+            </motion.div>
+
+            <Stack
+              component={motion.div}
+              variants={varFadeInRight}
+              direction="row"
+              spacing={1}
+              justifyContent={{ xs: 'center', md: 'flex-start' }}
+            >
+              <Box sx={{ borderRadius: '50%' }}>
+                {' '}
+                <img alt="NBK logo" src="/static/home/NBK_logo.jpg" width={70} height={60} />
+              </Box>
+
+              <Link
+                underline="always"
+                href="https://c3nguyenbinhkhiem.vinhlong.edu.vn/?fbclid=IwAR2KBheaoXqj6CP9T2EgrNpciNX77XV-XkOisxqE7RUYzTJHV1Kx229y7ek"
+                target="_blank"
+                color="common.white"
+                sx={{ typography: 'body2' }}
+              >
+                {/* Preview in Sketch Cloud */}
+                Trường THPT Chuyên Nguyễn Bỉnh Khiêm, Thành phố Vĩnh Long, tỉnh Vĩnh Long, Việt Nam
+              </Link>
+            </Stack>
+
+            <motion.div variants={varFadeInRight}>
+              <Button
+                size="large"
+                variant="contained"
+                component={RouterLink}
+                to={PATH_DASHBOARD.root}
+                startIcon={<Icon icon={flashFill} width={20} height={20} />}
+              >
+                {/* Live Preview */}
+                Liên hệ
+              </Button>
+            </motion.div>
+
+            <Stack
+              direction="row"
+              spacing={1.5}
+              justifyContent={{ xs: 'center', md: 'flex-start' }}
+            >
+              <motion.img variants={varFadeInRight} src="/static/home/facebook-svgrepo-com.svg" />
+              <motion.img variants={varFadeInRight} src="/static/home/website-svgrepo-com.svg" />
+              {/* <motion.img variants={varFadeInRight} src="/static/home/ic_m_material.svg" />
+              <motion.img variants={varFadeInRight} src="/static/home/ic_m_react.svg" />
+              <motion.img variants={varFadeInRight} src="/static/home/ic_m_js.svg" />
+              <motion.img variants={varFadeInRight} src="/static/home/ic_m_ts.svg" /> */}
+            </Stack>
+          </ContentStyle>
+        </Container>
+      </RootStyle>
+      <Box sx={{ height: { md: '100vh' } }} />
+    </>
+  );
+}
---
+++ src/components/_external-pages/landing/LandingMinimal.tsx
@@ -0,0 +1,254 @@
+// material
+import { alpha, useTheme, styled } from '@material-ui/core/styles';
+import { Icon } from '@iconify/react';
+import plusFill from '@iconify/icons-eva/plus-fill';
+import { useEffect, useCallback, useState } from 'react';
+import {
+  Box,
+  Grid,
+  Card,
+  Container,
+  Typography,
+  useMediaQuery,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+  Stack,
+  Skeleton
+} from '@material-ui/core';
+//
+
+import useSettings from 'hooks/useSettings';
+import HeaderBreadcrumbs from 'components/HeaderBreadcrumbs';
+import { PATH_DASHBOARD } from 'routes/paths';
+import { Link as RouterLink } from 'react-router-dom';
+import { BlogPostCard, BlogPostsSearch, BlogPostsSort } from 'components/_dashboard/blog';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMorePosts, getPostsInitial } from 'redux/slices/blog';
+import { orderBy } from 'lodash';
+import { varFadeInUp, MotionInView, varFadeInDown } from '../../animate';
+import { BlogState, Post } from '../../../@types/blog';
+
+// ----------------------------------------------------------------------
+
+const CARDS = [
+  {
+    icon: '/static/icons/ic_design.svg',
+    title: 'UI & UX Design',
+    description:
+      'The set is built on the principles of the atomic design system. It helps you to create projects fastest and easily customized packages for your projects.'
+  },
+  {
+    icon: '/static/icons/ic_code.svg',
+    title: 'Development',
+    description: 'Easy to customize and extend each component, saving you time and money.'
+  },
+  {
+    icon: '/static/brand/logo_single.svg',
+    title: 'Branding',
+    description: 'Consistent design in colors, fonts ... makes brand recognition easy.'
+  }
+];
+
+const SORT_OPTIONS = [
+  { value: 'latest', label: 'Latest' },
+  { value: 'popular', label: 'Popular' },
+  { value: 'oldest', label: 'Oldest' }
+];
+
+const shadowIcon = (color: string) => `drop-shadow(2px 2px 2px ${alpha(color, 0.48)})`;
+
+const RootStyle = styled('div')(({ theme }) => ({
+  paddingTop: theme.spacing(15),
+  [theme.breakpoints.up('md')]: {
+    paddingBottom: theme.spacing(15)
+  }
+}));
+
+const CardStyle = styled(Card)(({ theme }) => {
+  const shadowCard = (opacity: number) =>
+    theme.palette.mode === 'light'
+      ? alpha(theme.palette.grey[500], opacity)
+      : alpha(theme.palette.common.black, opacity);
+
+  return {
+    maxWidth: 380,
+    minHeight: 440,
+    margin: 'auto',
+    textAlign: 'center',
+    padding: theme.spacing(10, 5, 0),
+    boxShadow: `-40px 40px 80px 0 ${shadowCard(0.48)}`,
+    [theme.breakpoints.up('md')]: {
+      boxShadow: 'none',
+      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800]
+    },
+    '&.cardLeft': {
+      [theme.breakpoints.up('md')]: { marginTop: -40 }
+    },
+    '&.cardCenter': {
+      [theme.breakpoints.up('md')]: {
+        marginTop: -80,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: `-40px 40px 80px 0 ${shadowCard(0.4)}`,
+        '&:before': {
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: -1,
+          content: "''",
+          margin: 'auto',
+          position: 'absolute',
+          width: 'calc(100% - 40px)',
+          height: 'calc(100% - 40px)',
+          borderRadius: theme.shape.borderRadiusMd,
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: `-20px 20px 40px 0 ${shadowCard(0.12)}`
+        }
+      }
+    }
+  };
+});
+
+const CardIconStyle = styled('img')(({ theme }) => ({
+  width: 40,
+  height: 40,
+  margin: 'auto',
+  marginBottom: theme.spacing(10),
+  filter: shadowIcon(theme.palette.primary.main)
+}));
+
+const applySort = (posts: Post[], sortBy: string) => {
+  if (sortBy === 'latest') {
+    return orderBy(posts, ['createdAt'], ['desc']);
+  }
+  if (sortBy === 'oldest') {
+    return orderBy(posts, ['createdAt'], ['asc']);
+  }
+  if (sortBy === 'popular') {
+    return orderBy(posts, ['view'], ['desc']);
+  }
+  return posts;
+};
+
+const SkeletonLoad = (
+  <Grid container spacing={3} sx={{ mt: 2 }}>
+    {[...Array(4)].map((_, index) => (
+      <Grid item xs={12} md={3} key={index}>
+        <Skeleton variant="rectangular" width="100%" sx={{ height: 200, borderRadius: 2 }} />
+        <Box sx={{ display: 'flex', mt: 1.5 }}>
+          <Skeleton variant="circular" sx={{ width: 40, height: 40 }} />
+          <Skeleton variant="text" sx={{ mx: 1, flexGrow: 1 }} />
+        </Box>
+      </Grid>
+    ))}
+  </Grid>
+);
+
+// ----------------------------------------------------------------------
+
+export default function LandingMinimalHelps() {
+  // Tin tức & sự kiện
+  const dispatch = useDispatch();
+  const [filters, setFilters] = useState('latest');
+  const { posts, hasMore, index, step } = useSelector((state: { blog: BlogState }) => state.blog);
+
+  const sortedPosts = applySort(posts, filters);
+  const onScroll = useCallback(() => dispatch(getMorePosts()), [dispatch]);
+  const theme = useTheme();
+  const isLight = theme.palette.mode === 'light';
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+  const { themeStretch } = useSettings();
+  const handleChangeSort = (value?: string) => {
+    if (value) {
+      setFilters(value);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getPostsInitial(index, step));
+  }, [dispatch, index, step]);
+
+  return (
+    <RootStyle>
+      <Container maxWidth="lg">
+        <Box sx={{ mb: { xs: 10, md: 25 } }}>
+          <MotionInView variants={varFadeInUp}>
+            <Typography
+              component="p"
+              variant="overline"
+              sx={{ mb: 2, color: 'primary.main', textAlign: 'center' }}
+            >
+              N B K
+            </Typography>
+          </MotionInView>
+          <MotionInView variants={varFadeInDown}>
+            <Typography variant="h2" sx={{ textAlign: 'center', color: 'primary.main' }}>
+              Tin Tức & Sự kiện
+            </Typography>
+          </MotionInView>
+        </Box>
+
+        {/* <Grid container spacing={isDesktop ? 10 : 5}>
+          <Grid key="title" item xs={12} md={4}>
+            <MotionInView variants={varFadeInUp}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                      ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            </MotionInView>
+          </Grid>
+        </Grid> */}
+        <Container maxWidth={themeStretch ? false : 'lg'}>
+          <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
+            <Button
+              variant="outlined"
+              component={RouterLink}
+              to={PATH_DASHBOARD.blog.newPost}
+              // startIcon={<Icon icon={plusFill} />}
+            >
+              Xem tất cả
+            </Button>
+          </Stack>
+
+          <InfiniteScroll
+            next={onScroll}
+            hasMore={hasMore}
+            loader={SkeletonLoad}
+            dataLength={posts.length}
+            style={{ overflow: 'inherit' }}
+          >
+            <Grid container spacing={3}>
+              {sortedPosts.map((post, index) => (
+                <BlogPostCard key={post.id} post={post} index={index} />
+              ))}
+            </Grid>
+          </InfiniteScroll>
+        </Container>
+      </Container>
+    </RootStyle>
+  );
+}
