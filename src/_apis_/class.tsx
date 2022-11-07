48e8e21d9295d819e0b09cf1d66c40094ce89fbc
tranbason241@gmail.com <tranbason241@gmail.com>
2022-11-07 11:07:02 +0700

    update

---
+++ src/_apis_/class.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export class ClassManger {
+  // get list class
+  getListClass = (catetoryId = '') =>
+    axios
+      .get(`api/v1/classes/get-class-students?`, {
+        params: {
+          // page_size: p_size,
+          // page_number: p_number
+          StudentId: 1
+        }
+      })
+      .then((res) => res)
+      .catch((err) => err);
+  // get new by id
+
+  getNewById = (newID: string) =>
+    axios
+      .get(`/api/v1/news/${newID}`)
+      .then((res) => res)
+      .catch((err) => err);
+}
+export const manaClass = new ClassManger();
---
+++ src/redux/slices/class.ts
@@ -0,0 +1,93 @@
+import { map, filter } from 'lodash';
+import { createSlice } from '@reduxjs/toolkit';
+import { manageNew } from '_apis_/new';
+import { dispatch } from '../store';
+// utils
+import axios from '../../utils/axios';
+import { Class } from '../../@types/class';
+import { manaClass } from '_apis_/class';
+
+// ----------------------------------------------------------------------
+
+type NewState = {
+  isLoading: boolean;
+  error: boolean;
+  classList: Class[];
+};
+
+const initialState: NewState = {
+  isLoading: false,
+  error: false,
+  classList: []
+};
+
+const slice = createSlice({
+  name: 'class',
+  initialState,
+  reducers: {
+    // START LOADING
+    startLoading(state) {
+      state.isLoading = true;
+    },
+
+    // HAS ERROR
+    hasError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    // GET LIST CLASS
+
+    getListClass(state, action) {
+      state.isLoading = false;
+      state.classList = action.payload;
+    }
+  }
+});
+
+// Reducer
+export default slice.reducer;
+
+// Actions
+// export const { deleteGarden, deleteGardenType, deleteSite } = slice.actions;
+
+// ----------------------------------------------------------------------
+
+// get list new
+export function getListNew(categoryId: string) {
+  return async () => {
+    dispatch(slice.actions.startLoading());
+
+    try {
+      manaClass.getListClass(categoryId).then((response) => {
+        if (response.status == 200) {
+          dispatch(slice.actions.getListClass(response.data.items));
+          console.log(response.data.items);
+        } else {
+          dispatch(slice.actions.getListClass([]));
+        }
+      });
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
+
+// // get list new
+// export function getNewDetail(newId: string) {
+//   return async () => {
+//     dispatch(slice.actions.startLoading());
+//     try {
+//       manageNew.getNewById(newId).then((response) => {
+//         // console.log(response);
+//         if (response.status == 200) {
+//           dispatch(slice.actions.getNewDetail(response.data));
+//           console.log(response.data);
+//         }
+//       });
+//     } catch (error) {
+//       dispatch(slice.actions.hasError(error));
+//       console.log(error);
+//     }
+//   };
+// }
